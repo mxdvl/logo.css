b9a06ab03eeb2a37209d3e0cf3cb1f0bf94d608d
Max Duval <max.duval@hometrack.com>
2024-11-15 09:36:32 +0000

    feat: make it curvy

--- logo.mjs
+++ logo.mjs
@@ -1,40 +1,41 @@
-if (import.meta.main) {
-  console.log(draw());
-}
-
 /**
  * @param {object} options
  * @param {number} options.h1 vertical position of A, C, D & F
+ *
  * @param {number} options.h2 vertical position of B & E
  * @param {number} options.w1 horizontal position of A, C, D & F
  *
+ * @param {number} options.t1 outward control point of A, C, D & F
+ * @param {number} options.t2 horizontal control point of B & E
+ * @param {number} options.t3 inward control point of C & D
+ *
  * ```
- * .  ╋━w1━┫
- * ╭──╴B╶──╮  ┳
- * │       │  h2
+ * .  ╋━w1━┫            ┯ ┯ D F
+ * ╭──╴B╶──╮  ┳    t1 : │ │ │ │
+ * │       │  h2        A C ┷ ┷
  * C       A  ╋
- * │          h1
- * ╰───╂───╮  ┻
+ * │          h1   t2 : ┠─╴B╶─┨
+ * ╰───╂───╮  ┻         ┠─╴E╶─┨
  * .       │
- * F       D
- * │       │
- * ╰──╴E╶──╯
+ * F       D            C  ┯
+ * │       │       t3 : │  │
+ * ╰──╴E╶──╯            ┷  D
  * ```
  */
-function drawS({ h1, h2, w1 }) {
-  const A = /** @type {const} */ ([w1, -h1]);
-  const B = /** @type {const} */ ([0, -(h1 + h2)]);
-  const C = /** @type {const} */ ([-w1, -h1]);
-  const D = /** @type {const} */ ([w1, h1]);
-  const E = /** @type {const} */ ([0, h1 + h2]);
-  const F = /** @type {const} */ ([-w1, h1]);
+function drawS({ h1, h2, w1, t1, t2, t3 }) {
+  const [Ax, Ay] = [w1, -h1];
+  const [Bx, By] = [0, -(h1 + h2)];
+  const [Cx, Cy] = [-w1, -h1];
+  const [Dx, Dy] = [w1, h1];
+  const [Ex, Ey] = [0, h1 + h2];
+  const [Fx, Fy] = [-w1, h1];
   return [
-    `M ${A}`,
-    `L ${B}`,
-    `L ${C}`,
-    `L ${D}`,
-    `L ${E}`,
-    `L ${F}`,
+    `M ${[Ax, Ay]}`,
+    `C ${[Ax, Ay - t1]} ${[Bx + t2, By]} ${[Bx, By]}`,
+    `C ${[Bx - t2, By]} ${[Cx, Cy - t1]} ${[Cx, Cy]}`,
+    `C ${[Cx, Cy + t3]} ${[Dx, Dy - t3]} ${[Dx, Dy]}`,
+    `C ${[Dx, Dy + t1]} ${[Ex + t2, Ey]} ${[Ex, Ey]}`,
+    `C ${[Ex - t2, Ey]} ${[Fx, Fy + t1]} ${[Fx, Fy]}`,
   ].join(" \n");
 }
 
@@ -42,55 +43,83 @@ function drawS({ h1, h2, w1 }) {
  * @param {object} options
  * @param {number} options.h1 vertical position of A, C, D & F
  * @param {number} options.h2 vertical position of B & E
+ *
  * @param {number} options.w1 horizontal position of A, C, D & F
  *
+ * @param {number} options.t1 outward control point of A, C, D & F
+ * @param {number} options.t2 horizontal control point of B & E
+ *
  * ```
- * .  ╋━w1━┫
- * ╭──╴B╶──╮  ┳
- * │       │  h2
+ * .  ╋━w1━┫            ┯ ┯ D F
+ * ╭──╴B╶──╮  ┳    t1 : │ │ │ │
+ * │       │  h2        A C ┷ ┷
  * C       A  ╋
- * │          h1
- * │   ╋      ┻
+ * │          h1   t2 : ┠─╴B╶─┨
+ * │   ╋      ┻         ┠─╴E╶─┨
  * │
  * D       F
  * │       │
  * ╰──╴E╶──╯
  * ```
  */
-function drawC({ h1, h2, w1 }) {
-  const A = /** @type {const} */ ([w1, -h1]);
-  const B = /** @type {const} */ ([0, -(h1 + h2)]);
-  const C = /** @type {const} */ ([-w1, -h1]);
-  const D = /** @type {const} */ ([-w1, h1]);
-  const E = /** @type {const} */ ([0, h1 + h2]);
-  const F = /** @type {const} */ ([w1, h1]);
+function drawC({ h1, h2, w1, t1, t2 }) {
+  const [Ax, Ay] = [w1, -h1];
+  const [Bx, By] = [0, -(h1 + h2)];
+  const [Cx, Cy] = [-w1, -h1];
+  const [Dx, Dy] = [-w1, h1];
+  const [Ex, Ey] = [0, h1 + h2];
+  const [Fx, Fy] = [w1, h1];
   return [
-    `M ${A}`,
-    `L ${B}`,
-    `L ${C}`,
-    `L ${D}`,
-    `L ${E}`,
-    `L ${F}`,
+    `M ${[Ax, Ay]}`,
+    `C ${[Ax, Ay - t1]} ${[Bx + t2, By]} ${[Bx, By]}`,
+    `C ${[Bx - t2, By]} ${[Cx, Cy - t1]} ${[Cx, Cy]}`,
+    `L ${[Dx, Dy]}`,
+    `C ${[Dx, Dy + t1]} ${[Ex - t2, Ey]} ${[Ex, Ey]}`,
+    `C ${[Ex + t2, Ey]} ${[Fx, Fy + t1]} ${[Fx, Fy]}`,
   ].join(" \n");
 }
 
-export function draw() {
-  const w1 = 20;
-  const h1 = 30;
-  const h2 = 10;
-  const k = 5;
-  return `<svg viewBox="-120 -120 240 240" fill=none stroke=black>
-  <path class="C"
-    transform="translate(${w1 * 0 + k * 0} 0)"
-    d="${drawC({ h1, h2, w1 })}"
-  />
-  <path class="S"
-    transform="translate(${w1 * 2 + k * 1} 0)"
-    d="${drawS({ h1, h2, w1 })}"
-  />
-  <path class="S"
-    transform="translate(${w1 * 4 + k * 2} 0)"
-    d="${drawS({ h1, h2, w1 })}"
-  />
+/**
+ * Draw the CSS logo
+ *
+ * @param {string} foreground
+ * @param {string} background
+ * @returns {string} The SVG output
+ */
+export function draw(foreground, background) {
+  const thickness = 16;
+
+  const w1 = 18;
+
+  const h1 = 24;
+  const h2 = 24;
+
+  const t1 = 12;
+  const t2 = 12;
+  const t3 = 30;
+
+  const k = thickness + 6;
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-120 -120 240 240" width="240" fill="none">
+  <title>CSS</title>
+  <rect x="-120" y="-120" width="240" height="240" rx="${k * 1.5}" fill="${background}" />
+  <g class="CSS"
+    stroke-width="${thickness}" stroke="${foreground}"
+    transform="translate(${120 - k - w1} ${120 - h1 - h2 - k})"
+  >
+    <path class="C"
+      transform="translate(-${w1 * 4 + k * 2} 0)"
+      d="${drawC({ h1, h2, w1, t1, t2, t3 })}"
+    />
+    <path class="S"
+      transform="translate(-${w1 * 2 + k * 1} 0)"
+      d="${drawS({ h1, h2, w1, t1, t2, t3 })}"
+    />
+    <path class="S"
+      transform="translate(-${w1 * 0 + k * 0} 0)"
+      d="${drawS({ h1, h2, w1, t1, t2, t3 })}"
+    />
+  </g>
 </svg>`;
 }
+
+await Deno.writeTextFile("css.svg", draw("white", "rebeccapurple"));
